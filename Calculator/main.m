83358ea36a537541e5a1368fa10c50d2e6e63084
Rey <quit9909@icloud.com>
2015-02-28 23:54:33 +0900

    Initial Commit

---
+++ Calculator/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Calculator
+//
+//  Created by ReyMasuda on 2015/02/28.
+//  Copyright (c) 2015年 ReyMasuda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
